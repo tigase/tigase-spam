ecf9972c06667f0c03f5a6bc4c915a21cd8d4780
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2020-07-07 20:27:31 +0200

    Added documentation for implementation of a custom filter #spam-10

--- src/main/java/tigase/spam/SpamFilter.java
+++ src/main/java/tigase/spam/SpamFilter.java
@@ -22,18 +22,39 @@ import tigase.stats.StatisticsList;
 import tigase.xmpp.XMPPResourceConnection;
 
 /**
+ * Interface which needs to be implemented by all filters used by <code>SpamProcessor</code> to detect spam.
+ *
  * Created by andrzej on 08.04.2017.
  */
 public interface SpamFilter {
 
+	/**
+	 * Method processes packet and checks if it is a SPAM or not
+	 *
+	 * @return false if message should be dropped as it it a SPAM
+	 */
 	boolean filter(Packet packet, XMPPResourceConnection session);
 
+	/**
+	 * Method returns ID of a filter
+	 */
 	String getId();
 
+	/**
+	 * Method should fill the <code>list</code> parameter value with statistics about processed stanzas.
+	 * Default implementation should be used if there are no meaningful values to return.
+	 */
 	default void getStatistics(String name, StatisticsList list) {
 
 	}
 
+	/**
+	 * Method returns probability of detection of a spammer.
+	 * If value is closer to 1 this means that it is more likely that sender of stanza marked by this filter as a spam is a spammer and should be blocked.
+	 * This value is used by <code>ResultsAwareSpamFilter</code> implementation to decide if sender of a stanza should be blocked (ie. using number of blocked messages within a period of time and spammer detection probability returned by this method.
+	 *
+	 * @return values between 0 and 1
+	 */
 	default double getSpamProbability() {
 		return 1;
 	}
